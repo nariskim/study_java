233cdb13592beb2b8dc7e01d71dc09b3cf21e4fe
rhdee <rhdee@naver.com>
2022-02-03 16:11:07 +0900

    .

---
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -0,0 +1,10 @@
+package com.urdomain.lecture.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("What a world!");
+	}
+
+}
