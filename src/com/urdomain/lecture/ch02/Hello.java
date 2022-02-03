b498d4a054225cc0e805eee5db1cdab09fe9e87f
rhdee <rhdee@naver.com>
2022-02-03 16:48:11 +0900

    .

--- src/com/urdomain/lecture/ch02/Hello.java
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("What a world!");
+			
+			int a; // 선언
+			a=1; // 대입
+
+			
+			int b = 2;
+			int c = 0;
+			
+			c= a + b;
+			
+			System.out.println(c);
 	}
 
 }
