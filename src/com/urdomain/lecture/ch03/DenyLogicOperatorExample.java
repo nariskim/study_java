97723e7b4061da425ae165ebd0aa062854554adc
rhdee <rhdee@naver.com>
2022-02-09 17:33:35 +0900

    .

---
+++ src/com/urdomain/lecture/ch03/DenyLogicOperatorExample.java
@@ -0,0 +1,26 @@
+package com.urdomain.lecture.ch03;
+
+public class DenyLogicOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean play = true;
+		// boolean play1 = false;
+		
+		// boolean useNy = true; //1
+		// boolean delNy = false; //0
+		System.out.println(play);
+
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		
+		
+		
+	}
+
+}
