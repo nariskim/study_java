987f61f7599a574079787c4d75f5645da7d53ca4
rhdee <rhdee@naver.com>
2022-02-14 17:12:16 +0900

    .

--- src/com/urdomain/lecture/ch04/BreakExample.java
+++ src/com/urdomain/lecture/ch04/BreakExample.java
@@ -4,7 +4,15 @@ public class BreakExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		while(true) {
+			
+			int num = ((int)(Math.random()*6) +1);
+			System.out.println(num);
+			if(num == 6) {
+				break;
+			}
+		}
+		System.out.println("process ended!");
 	}
 
 }
