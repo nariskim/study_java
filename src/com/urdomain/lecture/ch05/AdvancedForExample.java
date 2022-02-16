3623659ca48b3533f7d453163162364fcc349aeb
rhdee <rhdee@naver.com>
2022-02-16 16:28:46 +0900

    .

--- src/com/urdomain/lecture/ch05/AdvancedForExample.java
+++ src/com/urdomain/lecture/ch05/AdvancedForExample.java
@@ -5,7 +5,18 @@ public class AdvancedForExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] scores= {95, 71, 84, 93, 87};
 		
+			for(int score : scores) {
+				
+				System.out.println("score : " + score);
+			}
+//			for(int i=0; i<scores.length; i++) {
+//				
+//				System.out.println(scores[i]);
+//			}
+				
+				
 		
 		
 	}
---
+++ src/com/urdomain/lecture/ch05/Week.java
@@ -0,0 +1,23 @@
+package com.urdomain.lecture.ch05;
+
+public enum Week {	//	Enumberation Type
+	
+	
+//	Enumberation constants : 열거 상수
+//	무조건 대문자
+//	복합문자는 언더바 사용 예) BLUE_MONDAY (스테이크표기법)
+	
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+	
+	
+	
+	
+	
+
+}
