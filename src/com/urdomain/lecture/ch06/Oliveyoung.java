12258de2e49b586a95848b5f0bb2714534b21127
rhdee <rhdee@naver.com>
2022-02-17 17:23:15 +0900

    .

---
+++ src/com/urdomain/lecture/ch06/Oliveyoung.java
@@ -0,0 +1,13 @@
+package com.urdomain.lecture.ch06;
+
+import java.util.Date;
+
+public class Oliveyoung {
+
+	
+	String skincare ="";
+	String eyemakeup ="";
+	String lipmakeup ="";
+	String cleanser ="";
+	Date orderdate;
+}
