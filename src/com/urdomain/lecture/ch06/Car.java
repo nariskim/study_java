a9be497f03e829a246ada930847a540efa403479
rhdee <rhdee@naver.com>
2022-02-17 17:16:56 +0900

    .

---
+++ src/com/urdomain/lecture/ch06/Car.java
@@ -0,0 +1,18 @@
+package com.urdomain.lecture.ch06;
+
+public class Car {
+	
+	
+//	field
+	
+	String company = "현대자동차";
+	String model = "아반떼";
+	String color = "red";
+	
+	int maxSpeed = 200;
+	int speed = 0;	
+	
+	
+//	method
+
+}
---
+++ src/com/urdomain/lecture/ch06/Member.java
@@ -0,0 +1,17 @@
+package com.urdomain.lecture.ch06;
+
+public class Member {
+
+	String name = "김나나";
+	String id = "rhdeee";
+	String pwd = "poiuy0987*";
+	String address = "강서구";
+	String email = "rhdee@naver.com";
+	String color = "오렌지";
+	String gender = "여";
+	
+	int zipcode = 98750;
+	int fax = 1324567;
+	int phone = 01033333333;
+	
+}
