256340d83d791f931a5189fcc5bd1568b66d2f8e
rhdee <rhdee@naver.com>
2022-03-02 18:15:27 +0900

    .

---
+++ src/com/urdomain/lecture/ch07/SportsCar.java
@@ -0,0 +1,16 @@
+package com.urdomain.lecture.ch07;
+
+public class SportsCar extends Car {
+
+	@Override
+	public void speedUp() {
+		// TODO Auto-generated method stub
+		super.speedUp();
+	}
+
+//	@Override
+//	public void stop() {
+//		super.stop();
+//	}
+
+}
