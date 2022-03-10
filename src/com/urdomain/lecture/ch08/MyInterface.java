1e8fddc1036711f68051bbb4a5aa54248a081426
rhdee <rhdee@naver.com>
2022-03-10 15:20:28 +0900

    .

---
+++ src/com/urdomain/lecture/ch08/MyInterface.java
@@ -0,0 +1,11 @@
+package com.urdomain.lecture.ch08;
+
+public interface MyInterface {
+
+	void method1();			//		public abstract 추상메서드 생략되어있음
+	
+//	default void method2() {
+//		System.out.println("MyInterface-method2() 실행");
+//	}
+}
+
